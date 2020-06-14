7b925747133d4618ce5dd80de0d549bfb126e4b4
Krishnasharma03 <59115049+Krishnasharma03@users.noreply.github.com>
2020-06-14 22:38:44 +0530

    Add files via upload

---
+++ IoT_HA.ino
@@ -0,0 +1,179 @@
+#define Light D2
+
+
+#define Fan D7
+
+
+#include <ThingSpeak.h>
+
+
+#include <ESP8266WiFi.h>
+
+
+char ssid[] = "neelimaj";
+
+
+char pass[] = "12345678";
+
+
+WiFiClient client;
+
+   unsigned long counterChannelNumber = 670394;
+
+
+const char * myCounterReadAPIKey = "94JAW5TR9Q7YP06Z";
+
+
+unsigned int counterFieldNumber = 1;
+
+
+unsigned int counterFieldNumber1 = 2;
+
+
+void setup() {
+
+
+Serial.begin(115200); // Initialize serial
+
+
+
+
+pinMode(Light,OUTPUT);
+
+
+pinMode(Fan,OUTPUT);
+
+
+WiFi.mode(WIFI_STA);
+
+
+ThingSpeak.begin(client); // Initialize ThingSpeak
+
+
+}
+
+
+void loop() {
+
+
+int statusCode = 0;
+
+
+if(WiFi.status() != WL_CONNECTED){
+
+
+Serial.print("Attempting to connect to SSID: ");
+
+while(WiFi.status() != WL_CONNECTED){
+
+
+WiFi.begin(ssid, pass);
+
+
+Serial.print(".");
+
+
+delay(5000);
+
+
+}
+
+
+
+
+Serial.println("\nConnected");
+
+
+}
+
+
+long count = ThingSpeak.readLongField(counterChannelNumber, counterFieldNumber, myCounterReadAPIKey);
+
+long count1 = ThingSpeak.readLongField(counterChannelNumber, counterFieldNumber1, myCounterReadAPIKey);
+
+statusCode = ThingSpeak.getLastReadStatus();
+
+
+Serial.println("Counter1: " + String(count1));
+
+
+if(statusCode == 200){
+
+
+Serial.println("Counter: " + String(count));
+
+
+}
+
+else{
+
+
+Serial.println("Problem reading channel. HTTP error code " + String(statusCode));
+
+
+}
+
+
+if(count==2)
+
+
+{
+
+
+Serial.println("2");
+
+
+digitalWrite(Light,HIGH);//BULB ON
+
+
+}
+else if(count==3)
+
+
+{
+
+
+Serial.println("3");
+
+
+digitalWrite(Light,LOW);//bulb off
+
+
+}
+
+
+else if(count==4)
+
+
+{
+
+
+
+Serial.println("4");
+
+
+digitalWrite(Fan,HIGH);//Fan ON
+
+
+}
+
+
+else if(count==5)
+
+
+{
+
+
+Serial.println("5");
+
+
+digitalWrite(Fan,LOW);//Fan off
+
+
+}
+
+
+}
+
+
+
